6706d8fa067ac59809617f3e468c31d66cccdbdf
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-28 22:08:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from program.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 bot = Client('mybot', bot_token = "1853498659:AAFdnSWzBEA4wqp8qpnViDKyvOSjV4y9-vU", api_id = 3020564, api_hash = "91c026fadfdc442f504a0bd3e5c8cd18")
 
 @bot.on_message(filters.command(['start']))
-def start(client, message)
+def start(client, message):
     message.reply(f"Hello {message.from_user.first_name} how are you",
     reply_markup = InlinekeyboardMarkup([[InlineKeyboardButton("Owner", url = "t.me/xxxtentacion_OF_TG")]]))
 
