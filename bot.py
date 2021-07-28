ed12507842f7315e8738c6f326113c7aaf803c21
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-28 22:34:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import os
 from pyrogram import Client as bot
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-bot = Client('mybot', bot_token = "1853498659:AAFdnSWzBEA4wqp8qpnViDKyvOSjV4y9-vU", api_id = "3020564", api_hash = "91c026fadfdc442f504a0bd3e5c8cd18")
 
 @bot.on_message(filters.command(['start']))
 def start(client, message):
