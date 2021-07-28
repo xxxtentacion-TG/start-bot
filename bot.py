6455ceacb2d9e649704623853cfd23993c5e8cd9
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-28 22:02:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from program import Client as bot
 from program import Filters
 from program.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 bot = Client('mybot', bot_token = "1853498659:AAFdnSWzBEA4wqp8qpnViDKyvOSjV4y9-vU", api_id = 3020564, api_hash = "91c026fadfdc442f504a0bd3e5c8cd18")
+
 @bot.on_message(filters.command(['start']))
 def start(client, message)
     message.reply(f"Hello {message.from_user.first_name} how are you",
