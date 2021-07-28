bfa9cf32eab5541bcb771fd1dca5f08e67300872
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-28 22:45:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
-@bot.on_message(filters.command(['start']))
+@Client.on_message(filters.command(['start']))
 def start(client, message):
     message.reply(f"Hello {message.from_user.first_name} how are you",
     reply_markup = InlinekeyboardMarkup([[InlineKeyboardButton("Owner", url = "t.me/xxxtentacion_OF_TG")]]))
