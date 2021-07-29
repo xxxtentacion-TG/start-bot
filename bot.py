a68bebd3d72640e73da8f30411dff5814e4ebea5
— Λsᴡɪɴ Rᴀᴊ — <82755354+Aswin-TG@users.noreply.github.com>
2021-07-29 09:32:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,26 @@
 import os
-from pyrogram import Client
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
-@Client.on_message(filters.command(['start']))
+bot = Client('mybot',
+      bot_token="1853498659:AAFdnSWzBEA4wqp8qpnViDKyvOSjV4y9-vU",
+      api_id=3020564,
+      api_hash="91c026fadfdc442f504a0bd3e5c8cd18",
+      )
+
+@bot.on_message(filters.command(['start']))
 def start(client, message):
     message.reply(f"Hello {message.from_user.first_name} how are you",
-    reply_markup = InlinekeyboardMarkup([[InlineKeyboardButton("Owner", url = "t.me/xxxtentacion_OF_TG")]]))
+    reply_markup = InlinekeyboardMarkup(
+      [
+         [
+            InlineKeyboardButton(
+            text ="Owner",
+            url = "t.me/xxxtentacion_OF_TG")
+         ]
+      ]
+      )
+      )
 
+bot.run()
 
