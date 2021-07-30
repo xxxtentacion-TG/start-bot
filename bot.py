ba6886afdc79d813d633d7134575105e2dec44cc
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-07-30 19:51:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,10 +15,10 @@ def start(client, message):
       [
          [
             InlineKeyboardButton('Owner', url='https://t.me/XXXTENTACION_OF_TG'),
-            InlineKeyboardButton('GROUP', url='https://t.me/MGMOVIEGRAM')
+            InlineKeyboardButton('group', url='https://t.me/MGMOVIEGRAM')
          ],
          [
-            InlineKeyboardButton('GROUP', url='https://t.me/MGMOVIEGRAM')
+            InlineKeyboardButton('help', callback_data="help"),
          ]
       ]
       )
